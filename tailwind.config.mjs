c9fbfe23033b9d43206302087a04ad394190e2e8
KJY <139238536+dani784601@users.noreply.github.com>
2024-12-31 16:46:08 +0900

    feat: ESLint 및 Prettier 설정 추가, 마켓플레이스 페이지 레이아웃 구현

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,18 +7,23 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily :{
-        sans: ['Noto Sans KR', 'sans-serif'],
-        baskin: ['BaskinRobbins', 'Noto Sans KR'],  //  className="font-baskin" 으로 사용 
+      fontFamily: {
+        sans: ["Noto Sans KR", "sans-serif"],
+        baskin: ["BaskinRobbins", "Noto Sans KR"], //  className="font-baskin" 으로 사용
       },
-
-      colors: { //시그마 디자인 시안에서 확인하고 커스텀으로 만든 컬러들입니다
-            //className="bg-customColor"
-        customRed: '#FF483D',   // RED
-        customBlue: '#29C9F9',  // BLUE
-        customPurple: '#A77EFF', //PURPLE
-        customPink: '#FF2A6A',   //PINK
-        customMain: '#EFFF04',  //main (노란색)
+      screens: {
+        mb: "375px",
+        tb: "744px",
+        lt: "1200px",
+      },
+      colors: {
+        //시그마 디자인 시안에서 확인하고 커스텀으로 만든 컬러들입니다
+        //className="bg-customColor"
+        customRed: "#FF483D", // RED
+        customBlue: "#29C9F9", // BLUE
+        customPurple: "#A77EFF", //PURPLE
+        customPink: "#FF2A6A", //PINK
+        customMain: "#EFFF04", //main (노란색)
       },
     },
   },
