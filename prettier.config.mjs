2539bfa214fda72ae24c075303415d0af4e12119
Kim Doobong <tengal73@naver.com>
2025-01-03 09:30:05 +0900

    1.Tailwind CSS IntelliSense Extension 설치. 2. 	prettier-plugin-tailwindcss 설치  - a. 	npm install -D prettier prettier-plugin-tailwindcss  - b. 	// prettier.config.js 파일에 	plugins: ["prettier-plugin-tailwindcss"], // TailwindCSS 플러그인 추가

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,11 +1,12 @@
 const config = {
   printWidth: 100,
-  trailingComma: "all", // 기본값
+  trailingComma: 'all', // 기본값
   tabWidth: 2, // 기본값
   semi: true, // 일부 코드에서 라인의 시작 부분에 세미 콜론 추가
   singleQuote: true,
   bracketSpacing: true, // 기본값. true인 경우 {foo:bar}는 { foo: bar }로 변환됨
-  arrowParens: "always", // 기본값
+  arrowParens: 'always', // 기본값
   useTabs: false, // 기본값
+  plugins: ['prettier-plugin-tailwindcss'], // TailwindCSS 플러그인 추가
 };
 export default config;
