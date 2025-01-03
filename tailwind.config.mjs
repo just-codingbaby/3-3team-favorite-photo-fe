ee8f979342893ecdea64ba1c4d28bb8d2736d2a6
KJY <139238536+dani784601@users.noreply.github.com>
2025-01-03 17:46:34 +0900

    feat: add shadcn/ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,31 +1,81 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Noto Sans KR", "sans-serif"],
-        baskin: ["BaskinRobbins", "Noto Sans KR"], //  className="font-baskin" 으로 사용
-      },
-      screens: {
-        mb: "375px",
-        tb: "744px",
-        lt: "1200px",
-      },
-      colors: {
-        //시그마 디자인 시안에서 확인하고 커스텀으로 만든 컬러들입니다
-        //className="bg-customColor"
-        customRed: "#FF483D", // RED
-        customBlue: "#29C9F9", // BLUE
-        customPurple: "#A77EFF", //PURPLE
-        customPink: "#FF2A6A", //PINK
-        customMain: "#EFFF04", //main (노란색)
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			sans: [
+  				'Noto Sans KR',
+  				'sans-serif'
+  			],
+  			baskin: [
+  				'BaskinRobbins',
+  				'Noto Sans KR'
+  			]
+  		},
+  		screens: {
+  			mb: '375px',
+  			tb: '744px',
+  			lt: '1200px'
+  		},
+  		colors: {
+  			customRed: '#FF483D',
+  			customBlue: '#29C9F9',
+  			customPurple: '#A77EFF',
+  			customPink: '#FF2A6A',
+  			customMain: '#EFFF04',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
