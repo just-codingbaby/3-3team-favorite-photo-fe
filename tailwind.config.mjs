9daa6661fae34e6e6548f05a2d55dd4cba04529e
KJY <139238536+dani784601@users.noreply.github.com>
2025-01-06 17:06:39 +0900

    fix: type

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -65,9 +65,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: "var(--radius) / 2",
+        lg: "calc(var(--radius) / 2)",
         md: "calc(var(--radius) / 4)",
-        sm: "calc(var(--radius)) / 8",
+        sm: "calc(var(--radius) / 8)",
       },
     },
   },
