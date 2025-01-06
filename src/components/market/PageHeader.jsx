66cbcf0838b60a64a80e90511108d1183aa7cced
KJY <139238536+dani784601@users.noreply.github.com>
2025-01-06 16:16:23 +0900

    feat: Update PageHeader for enhanced filtering options

--- src/components/market/PageHeader.jsx
+++ src/components/market/PageHeader.jsx
@@ -1,6 +1,70 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const FILLTER_LIST = [
+  {
+    label: "등급",
+    category: "rate",
+    options: [
+      {
+        value: "common",
+        label: "COMMON",
+      },
+      {
+        value: "rare",
+        label: "RARE",
+      },
+      {
+        value: "superRare",
+        label: "SUPER RARE",
+      },
+      {
+        value: "legendary",
+        label: "LEGENDARY",
+      },
+    ],
+  },
+  {
+    label: "장르",
+    category: "genre",
+    options: [
+      {
+        value: "landscape",
+        label: "풍경",
+      },
+      {
+        value: "people",
+        label: "인물",
+      },
+      {
+        value: "object",
+        label: "사물",
+      },
+    ],
+  },
+  {
+    label: "매진 여부",
+    category: "status",
+    options: [
+      {
+        value: "onSale",
+        label: "판매 중",
+      },
+      {
+        value: "soldOut",
+        label: "판매 완료",
+      },
+    ],
+  },
+];
 
 export default function PageHeader() {
   return (
@@ -12,9 +76,9 @@ export default function PageHeader() {
             나의 포토카드 판매하기
           </Button>
         </div>
-        <div className="flex justify-between py-5">
-          <div className="flex gap-[30px]">
-            <label className="flex gap-1 border-white border">
+        <div className="py-5 grid grid-flow-col gap-1">
+          <div className="grid grid-flow-col gap-1">
+            <label className="flex gap-1 border w-full">
               <Input
                 className="border-0 focus-visible:outline-none focus-visible:ring-inset bg-none"
                 type="search"
@@ -26,34 +90,39 @@ export default function PageHeader() {
                 <Search size="24" />
               </Button>
             </label>
-            <span className="flex *:border *:border-black ">
-              <select name="rating">
-                <option value="">등급</option>
-                <option value="common">COMMON</option>
-                <option value="rare">RARE</option>
-                <option value="super-rare">SUPER RARE</option>
-                <option value="legendary">LEGENDARY</option>
-              </select>
-              <select name="genre">
-                <option>장르</option>
-                <option>풍경</option>
-                <option>인물</option>
-                <option>사물</option>
-              </select>
-              <select name="status">
-                <option>매진여부</option>
-                <option>판매 중</option>
-                <option>판매 완료</option>
-              </select>
-            </span>
+
+            <div className="flex gap-2">
+              {FILLTER_LIST.map((selectBox) => {
+                return (
+                  <Select key={selectBox.category}>
+                    <SelectTrigger className="w-[120px] border-none">
+                      <SelectValue placeholder={selectBox.label} />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {selectBox.options.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                );
+              })}
+            </div>
           </div>
-          <div className="contents">
-            <select name="order">
-              <option>최신 순</option>
-              <option>오래된 순</option>
-              <option>높은 가격순</option>
-              <option selected>낮은 가격순</option>
-            </select>
+
+          <div className="ml-auto">
+            <Select>
+              <SelectTrigger className="border w-[180px]">
+                <SelectValue placeholder="정렬" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="latest">최신 순</SelectItem>
+                <SelectItem value="oldest">오래된 순</SelectItem>
+                <SelectItem value="highPrice">높은 가격순</SelectItem>
+                <SelectItem value="lowPrice">낮은 가격순</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
         </div>
       </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,81 +1,75 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: [
-  				'Noto Sans KR',
-  				'sans-serif'
-  			],
-  			baskin: [
-  				'BaskinRobbins',
-  				'Noto Sans KR'
-  			]
-  		},
-  		screens: {
-  			mb: '375px',
-  			tb: '744px',
-  			lt: '1200px'
-  		},
-  		colors: {
-  			customRed: '#FF483D',
-  			customBlue: '#29C9F9',
-  			customPurple: '#A77EFF',
-  			customPink: '#FF2A6A',
-  			customMain: '#EFFF04',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans KR", "sans-serif"],
+        baskin: ["BaskinRobbins", "Noto Sans KR"],
+      },
+      screens: {
+        mb: "375px",
+        tb: "744px",
+        lt: "1200px",
+      },
+      colors: {
+        customRed: "#FF483D",
+        customBlue: "#29C9F9",
+        customPurple: "#A77EFF",
+        customPink: "#FF2A6A",
+        customMain: "#EFFF04",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)/2",
+        md: "calc(var(--radius)/4)",
+        sm: "calc(var(--radius))/8",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
