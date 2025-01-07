ae84a05cf313b6fc5d473ff50d397bbea0c7dff2
KJY <139238536+dani784601@users.noreply.github.com>
2025-01-07 13:27:23 +0900

    feat: 메인 경로 리다이렉트 설정(/->/market)

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/market",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
