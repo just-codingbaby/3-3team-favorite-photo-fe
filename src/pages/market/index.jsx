2d85ad6de47d16df853dbc08b795aab7cc670ccd
KJY <139238536+dani784601@users.noreply.github.com>
2025-01-10 09:46:48 +0900

    feat: aria-label 추가

--- src/pages/market/index.jsx
+++ src/pages/market/index.jsx
@@ -1,6 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import {useQuery} from "@tanstack/react-query";
 import PageHeader from "@/components/market/PageHeader";
-import { ProductCard } from "@/components/market/ProductCard";
+import {ProductCard} from "@/components/market/ProductCard";
 import Link from "next/link";
 
 export default function MarketPage() {
@@ -26,7 +26,10 @@ export default function MarketPage() {
         <section>
           <div className="grid grid-cols-2 gap-[5px] tb:gap-5 lt:grid-cols-3 lt:gap-20">
             {data.map((card) => (
-              <Link href={`/market/${card.id}`} key={card.id} >
+              <Link href={`/market/${card.id}`}
+                    key={card.id}
+                    className="block"
+                    aria-label={`${card.name} 카드 상세보기`}>
                 <ProductCard cardProps={card} />
               </Link>
             ))}
