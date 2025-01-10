191457e8f65ffb0d7ad4c32b25fc1a702d7c4050
KJY <139238536+dani784601@users.noreply.github.com>
2025-01-10 17:31:12 +0900

    feat: Add SSR support with React Query and improve data fetching

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,27 @@
 import "@/styles/globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import Layout from "@/components/layout";
+import {useState} from "react";
 
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  )
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
-
   return (
     <QueryClientProvider client={queryClient}>
-      
-      {getLayout(<Component {...pageProps} />)}
-      
+        {getLayout(<Component {...pageProps} />)}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
--- src/pages/market/index.jsx
+++ src/pages/market/index.jsx
@@ -1,39 +1,62 @@
-import {useQuery} from '@tanstack/react-query';
+import {dehydrate, HydrationBoundary, QueryClient, useQuery,} from '@tanstack/react-query'
+
 import PageHeader from '@/components/market/PageHeader';
 import {ProductCard} from '@/components/market/ProductCard';
 import Link from 'next/link';
+import {error} from "next/dist/build/output/log";
 
-export default function MarketPage() {
-  const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1';
-  const { isPending, error, data, _isFetching } = useQuery({
+async function getCards() {
+  try {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL+'/shop/cards');
+    return response.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient()
+  await queryClient.prefetchQuery({
     queryKey: ['cards'],
-    queryFn: async () => {
-      const response = await fetch(`${API_URL}/shop/cards`);
-      return await response.json();
+    queryFn: getCards
+  })
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
     },
-  });
+  }
+}
+export default function MarketPage({ dehydratedState }) {
+  // const API_URL = process.env.NEXT_PUBLIC_API_URL || '';
+  const { data, isPending, isError } = useQuery({
+    queryKey: ['cards'],
+    queryFn: getCards,
+  })
 
   if (isPending) return 'Loading...';
 
-  if (error) return 'An error has occurred: ' + error.message;
+  if (isError) return 'An error has occurred: ' + error.message;
+
   return (
-    <article className=" tb:container mx-auto px-[15px] tb:px-5">
-      <PageHeader />
-      <section>
-        <div className="grid grid-cols-2 gap-[5px] tb:gap-5 lt:grid-cols-3 lt:gap-20">
-          {data.map((card) => (
-            <Link
-              href={`/market/${card.id}`}
-              key={card.id}
-              className="block"
-              aria-label={`${card.name} 카드 상세보기`}
-              onClick={(e) => {e.stopPropagation()}}
-            >
-              <ProductCard cardProps={card} />
-            </Link>
-          ))}
-        </div>
-      </section>
-    </article>
+    <HydrationBoundary state={dehydratedState}>
+      <article className=" tb:container mx-auto px-[15px] tb:px-5">
+        <PageHeader />
+        <section>
+          <div className="grid grid-cols-2 gap-[5px] tb:gap-5 lt:grid-cols-3 lt:gap-20">
+            {data?.cards?.map((card) => (
+              <Link
+                href={`/market/${card.id}`}
+                key={card.id}
+                className="block"
+                aria-label={`${card.name} 카드 상세보기`}
+                onClick={(e) => {e.stopPropagation()}}
+              >
+                <ProductCard cardProps={card} />
+              </Link>
+            ))}
+          </div>
+        </section>
+      </article>
+    </HydrationBoundary>
   );
 }
